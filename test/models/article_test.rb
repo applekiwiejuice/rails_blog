7648fe776516f46fd69e22e55943a1946bb97c82
mcndev <mcnikkopinlacnetflix@gmail.com>
2021-02-24 03:43:39 +0800

    DELETE now working

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,27 @@
-require "test_helper"
+require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should not save when title is blank' do
+    article = Article.new(title: "", description: "1234567890")
+    assert_not article.save
+    assert_equal article.errors.full_messages.to_sentence, "Title can't be blank"
+  end
+
+  test 'should not save when description is blank' do
+    article = Article.new(title: "Lego: The Movie", description: "")
+    assert_not article.save
+    assert_equal article.errors.full_messages.to_sentence, "Description can't be blank and Description is too short (minimum is 10 characters)"
+  end
+
+  test 'should not save when description is less than 10 characters' do
+    article = Article.new(title: "Lego: The Movie", description: "123456")
+    assert_not article.save
+    assert_equal("Description is too short (minimum is 10 characters)", article.errors.full_messages.to_sentence)
+  end
+
+  test 'should save when everything is awesome' do
+    article = Article.new(title: "Lego: The Movie", description: "This is a Lego Movie.")
+    assert article.save
+  end
 end
