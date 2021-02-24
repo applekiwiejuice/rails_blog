db17df2a12331b18895c970a63c6bf0b23efde95
mcndev <mcnikkopinlacnetflix@gmail.com>
2021-02-25 02:20:23 +0800

    Fixed Minitest error by adding rexml gem, added models and controllers test for TDD activity

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,8 +1,64 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get articles_index_url
+  setup do
+    @article = articles(:one)
+  end
+
+  test "should get root path" do
+    get root_path
     assert_response :success
   end
+
+  test "should respond with HTTP code 200 when request goes to GET /articles" do
+    get articles_path
+    assert_equal response.code, "200"
+    # assert_response :success
+  end
+
+  test "should respond with redirect when a valid request goes to POST /articles" do
+    post articles_path, params: { article: { title: "Silicon Valley", body: "This is an awesome TV series" } }
+    assert_equal response.code, "302"
+    # assert_response :redirect
+  end
+
+  test "should respond with render when an invalid request goes to POST /articles" do
+    post articles_path, params: { article: { title: "Lego: The Movie", body: "" } }
+    assert_equal(response.code, "200")
+    # assert_response :success
+  end
+
+  test "should respond with HTTP code 200 when request goes to GET /articles/new" do
+    get new_article_path
+    assert_response :success
+  end
+
+  test "should respond with HTTP code 200 when request goes to GET /articles/:id/edit" do
+    get edit_article_path(@article)
+    assert_response :success
+  end
+
+  test "should respond with HTTP code 200 when request goes to GET /articles/:id" do
+    get article_path(@article)
+    assert_response :success
+  end
+
+  test "should respond with redirect when a valid request goes to PATCH /articles/:id" do
+    patch article_path(@article), params: { article: { title: "Silicon Valley: The TV Series", body: "This is an awesome TV series from HBO" } }
+    assert_equal response.code, "302"
+    # assert_response :redirect
+  end
+
+  test "should respond with redirect when a valid request goes to PUT /articles/:id" do
+    put article_path(@article), params: { article: { body: "This is an awesome TV series from HBO" } }
+    assert_equal response.code, "302"
+    # assert_response :redirect
+  end
+
+  test "should respond with redirect when a valid request goes to DELETE /articles/:id" do
+    delete article_path(@article)
+    assert_equal response.code, "302"
+    # assert_response :redirect
+  end
+
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -3,25 +3,25 @@ require 'test_helper'
 class ArticleTest < ActiveSupport::TestCase
 
   test 'should not save when title is blank' do
-    article = Article.new(title: "", description: "1234567890")
+    article = Article.new(title: "Not blank", body: "1234567890")
     assert_not article.save
     assert_equal article.errors.full_messages.to_sentence, "Title can't be blank"
   end
 
   test 'should not save when description is blank' do
-    article = Article.new(title: "Lego: The Movie", description: "")
+    article = Article.new(title: "Lego: The Movie", body: "")
     assert_not article.save
     assert_equal article.errors.full_messages.to_sentence, "Description can't be blank and Description is too short (minimum is 10 characters)"
   end
 
   test 'should not save when description is less than 10 characters' do
-    article = Article.new(title: "Lego: The Movie", description: "123456")
+    article = Article.new(title: "Lego: The Movie", body: "123456")
     assert_not article.save
     assert_equal("Description is too short (minimum is 10 characters)", article.errors.full_messages.to_sentence)
   end
 
   test 'should save when everything is awesome' do
-    article = Article.new(title: "Lego: The Movie", description: "This is a Lego Movie.")
+    article = Article.new(title: "Lego: The Movie", body: "This is a Lego Movie.")
     assert article.save
   end
 end
